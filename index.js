baac0bea1f66a25bec90d93b7102019848b0f71d
Jatupon Jantri <jameball007@gmail.com>
2018-01-24 13:52:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, { [key] :value })
 }
 
+function updateObjectWithKeyAndValue(object, key, value) {
+  var update = Object,assign(object, key, value)
+  return update
+}
+
